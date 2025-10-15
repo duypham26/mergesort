b5165fa3e621699e692fb1a4567f6837587794c6
duypham26~ <duyqwerty20@gmail.com~>
2025-10-15 10:52:56 +0700

    second commit

--- bai_3_phan_2.cpp
+++ bai_3_phan_2.cpp
@@ -20,6 +20,19 @@ int tim_min(int a[],int l, int r)
 }
 int main()
 {
-    int a[5] = {1,2,4-22,5};
+    int a[5] = {1,2,4,-22,5};
     cout <<tim_min(a,0,4);
 }
+/*
+giai thich:
+cho mảng a gồm n phần tử, ta sẽ chia mảng thành 2 nửa trái và phải
+sau đó ta sẽ tiếp tục chia 2 nửa này thành các nửa nhỏ hơn
+với mảng a[5] = {1,2,4,-22,5}
+ta sẽ chia thành 2 nửa [1,2,4] và [-22,5]
+tiếp tục chia [1,2] và [4], [-22] và [5]
+tiếp tục chia [1] và [2], [4], [-22], [5]
+khi mảng chỉ còn 1 phần tử thì so sánh với nhau, trả về phần tử nhỏ hơn
+với [1] và [2] trả về 1, với [4] trả về 4, với [-22] trả về -22, với [5] trả về 5
+tiếp tục so sánh [1] và [4] trả về 1, với [-22] và [5] trả về -22
+cuối cùng so sánh 1 và -22 trả về -22
+*/
